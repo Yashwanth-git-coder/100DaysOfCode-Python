6eb5e40008f26242b1be8a08b537353bfc566520
Yashwanth S <112617691+Yashwanth-git-coder@users.noreply.github.com>
2023-08-04 21:28:47 +0530

    Add files via upload

---
+++ 1. Day-1 Coding/project.py
@@ -0,0 +1,6 @@
+""" Band Name Generator """
+
+print("Welcom to Brand Name Generator")
+city_name = input("Enter Your City Name : ")
+pet_Name = input("Enter Your Pet Name : ")
+print(f"Brand name is {city_name} {pet_Name}")
